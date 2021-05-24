f2046b86875011aad0fc182fcb92bc1d55453afe
Lars-Woelki <71006155+Lars-Woelki@users.noreply.github.com>
2021-05-24 17:59:22 +0200

    airplane

--- Testing etc/Function.js
+++ Testing etc/Function.js
@@ -1,12 +1,22 @@
-var okbir = document.getElementsByClassName('ribko');
+var Flugzeug= document.getElementsByClassName('Flugzeug');
 function walk(){
 	
 	var value = window.scrollY;
-	console.log(value);
-	
-	okbir[0].style.right = value*2 + 'vw';
-	okbir[0].style.top = 0.05*value**2 + 'vw';
-	okbir[0].style.transform = "rotate("+ -2.3*value + "deg)";
+	Flugzeug[0].style.right = ((value*2)-30) + 'vw';
+	Flugzeug[0].style.top = 0.05*value**2 + 'vw';
+	if(parseInt((Flugzeug[0].style.right.substr(0,Flugzeug[0].style.right.length-2))) <20){
+		
+		Flugzeug[0].style.transform = "rotate("+ -2.3*value + "deg)";
+		Flugzeug[0].style.top = 0.05*value**2 + 'vw';
+		console.log(0.05*value**2);
+
+	}
+	else if(parseInt((Flugzeug[0].style.right.substr(0,Flugzeug[0].style.right.length-2))) >= 46){
+		console.log('>20');
+		Flugzeug[0].style.transform = "rotate(0deg)";
+		Flugzeug[0].style.top = '73' + 'vw';
+
+	}
 	
 	
 	
